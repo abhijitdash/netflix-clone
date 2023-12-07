0d8b89d47793e0a8c37f502fcc3aefdf55c8d149
Abhijit Dash <abhidash80@gmail.com>
2023-12-07 18:40:59 +0530

    navbar and scrolling

---
+++ netflix-ui/src/home/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./home.scss";
+import { AcUnit } from "@material-ui/icons"
+import Navbar from '../component/navbar/Navbar';
+
+const home = () => {
+  return (
+    <div className='home'>
+        <Navbar />
+        <img  width= "100%" src='https://images.pexels.com/photos/3560168/pexels-photo-3560168.jpeg?auto=compress&cs=tinysrgb&w=600' alt=""/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        vLorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.vLorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
+
+    </div>
+  )
+}
+
+export default home
