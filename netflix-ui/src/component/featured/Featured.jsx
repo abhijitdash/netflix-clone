457476e7d211881a4531ad1af75bbdbd30fb6aef
Abhijit Dash <abhidash80@gmail.com>
2023-12-08 11:36:10 +0530

    genre dropdowns and scrolls for top 10

---
+++ netflix-ui/src/component/featured/Featured.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import "./feature.scss"
+import { InfoOutlined, PlayArrow } from '@material-ui/icons'
+
+export default function Featured({ type}) {
+  return (
+    <div className='featured'>
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option > Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
+        <img width= "100%" src="https://prod-images.tcm.com/Master-Profile-Images/theshawshankredemption1994.20338.2.jpg?w=1216" alt="" />
+        <div className='info'>
+          <img src='https://img.resized.co/theslicedpan/eyJkYXRhIjoie1widXJsXCI6XCJodHRwczpcXFwvXFxcL21lZGlhLmJhbGxzLmllXFxcL3VwbG9hZHNcXFwvMjAxNVxcXC8wOFxcXC8wMjExNTcyMVxcXC9zaGF3c2hhbmstcmVkZW1wdGlvbi0xMDI0eDU2NS5qcGdcIixcIndpZHRoXCI6NjQwLFwiaGVpZ2h0XCI6MzYwLFwiZGVmYXVsdFwiOlwiaHR0cHM6XFxcL1xcXC93d3cudGhlc2xpY2VkcGFuLmNvbVxcXC9hc3NldHNcXFwvaVxcXC9uby1pbWFnZS5wbmc_dj0xXCJ9IiwiaGFzaCI6ImM1NjZmYTA5YTZlY2VkZDEwYTc1YTU2ZWNmZGE0OTEzZjA0OTg0NWEifQ==/quiz-how-much-do-you-remember-about-the-classic-shawshank-redemption.jpg' alt=''/>
+        <span className='desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         Alias magni quidem optio rerum, odio nesciunt ducimus, 
+         similique ab nobis incidunt beatae? Eius, 
+         voluptates reprehenderit perferendis doloribus ducimus pariatur voluptatibus a.
+         </span>
+         <div className='buttons'>
+          <button className='play'> 
+          <PlayArrow />
+          <span>Play</span>
+          </button>
+          <button className='more'>
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+         </div>
+        </div>
+        </div>
+  )
+}
--- netflix-ui/src/home/Home.jsx
+++ netflix-ui/src/home/Home.jsx
@@ -2,36 +2,19 @@ import React from 'react'
 import "./home.scss";
 import { AcUnit } from "@material-ui/icons"
 import Navbar from '../component/navbar/Navbar';
+import Featured from '../component/featured/Featured';
+import List from '../component/list/List';
 
 const home = () => {
   return (
     <div className='home'>
         <Navbar />
-        <img  width= "100%" src='https://images.pexels.com/photos/3560168/pexels-photo-3560168.jpeg?auto=compress&cs=tinysrgb&w=600' alt=""/>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        vLorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.vLorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rem aut, voluptatum dignissimos id omnis non enim repellat perspiciatis consectetur. Quia consectetur a perferendis velit esse accusantium est nam modi.
-
+        <Featured />
+        <List />
+        <List />
+        <List />
+        <List />
+        <List />
     </div>
   )
 }
