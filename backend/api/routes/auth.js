ae61a0914b962ba4ef98c461ebe78800557910d4
Abhijit Dash <abhidash80@gmail.com>
2023-12-30 11:00:01 +0530

    api side complete

--- backend/api/routes/auth.js
+++ backend/api/routes/auth.js
@@ -1,45 +1,52 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
-//register the user
+//REGISTER
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
-    });
-    try {
-        const user = await newUser.save();
-        res.status(201).json(user);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-
-//Log-In
+//LOGIN
 router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.body.email })
-        
-        if (!user)
-            return res.status(401).json("Wrong Password or username")
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) 
+        return res.status(401).json("Wrong password or username!");
 
-        //validating the password
-        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY)
-        const orig_password = bytes.toString(CryptoJS.enc.Utf8)
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-        if(orig_password !== req.body.password)
-        return res.status(401).json("Wrong Password")
+    if(originalPassword !== req.body.password)
+      return res.status(401).json("Wrong password or username!");
 
-        const { password, ...info } =user._doc
-        
-        return res.status(200).json(user)
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
 
-    } catch (err) {
-        return res.status(500).json(err)
-    }
-})
+    const { password, ...info } = user._doc;
+
+    return res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
---
+++ backend/api/routes/list.js
@@ -0,0 +1,111 @@
+const router = require("express").Router();
+const List = require("../models/List");
+const verify = require("../verifyToken");
+
+//Create movie/series list
+router.post("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        const newList = new List(req.body);
+
+        try {
+            const savedList = await newList.save();
+            res.status(201).json(savedList);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed");
+    }
+});
+
+//Update
+router.put("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const updatedMovie = await Movie.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
+
+            res.status(200).json(updatedMovie);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed");
+    }
+});
+
+//delete
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await List.findByIdAndDelete(req.params.id);
+
+            res.status(200).json("The List has been deleted...");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed");
+    }
+});
+
+//GET
+router.get("/", verify, async (req, res) => {
+        const typeQuery = req.query.type;
+        const genreQuery = req.query.genre;
+        let list = [];
+    try {
+        if(typeQuery){
+            //means we are in some movie/series page
+            if(genreQuery){
+                //some genre is chosen
+                list = await List.aggregate([
+                    {$sample: {size: 10 }},
+                    { $match: { type: typeQuery, genre: genreQuery }}
+                ])
+            }else{
+                list = await List.aggregate([
+                    {$sample: {size: 10 }},
+                    { $match: { type: typeQuery }}
+                ])
+            }
+        }else{
+            //in home page and can fetch random lists
+            list = await List.aggregate([{ $sample: { size: 10 }}])
+        }
+        res.status(200).json(list);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET random movies
+router.get("/random", verify, async (req, res) => {
+    console.log("coming........");
+    const type = req.query.type;
+    let movie;
+    try {
+        if (type === "series") {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: true } },
+                { $sample: { size: 1 } },
+            ]);
+        } else {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: false } },
+                { $sample: { size: 1 } },
+            ]);
+        }
+        res.status(200).json(movie);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
